a4c9d0b22098807df70ee31495da378477bf8c26
dpayton92 <dpayton92@gmail.com>
2020-09-16 17:26:30 -0700

    update app

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,10 +6,10 @@ const path = require ("path");
     // GET Requests
     module.exports = function(app) {
     app.get("/notes", function(req, res) {
-      res.sendFile(path.join(__dirname, "../Develop/public/index"));
+      res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
     });
   
     app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../Develop/public/index"));
+      res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
     });
   };
